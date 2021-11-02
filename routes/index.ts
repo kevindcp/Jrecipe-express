00b2afb14499ace814ae8079605089a3e5e72fc8
kevindcp <13-10268@usb.ve>
2021-11-02 02:53:46 -0400

    user controller

--- routes/index.ts
+++ routes/index.ts
@@ -1,31 +1,13 @@
 import express from "express"
 import { PrismaClient, Prisma } from '@prisma/client'
-import * as bcrypt from "bcrypt"
+import userRouter from "../controllers/user"
 const prisma = new PrismaClient()
 const router = express.Router()
-
 
 router.get('/',async (req, res) => {
     res.send('<h1> API </h1>')
 })
 
-router.post('/register', async(req, res) => {
-    const {name, email, password} = req.body
-    const passwordHash = await bcrypt.hash(password, 10)
-    const createdUser = await prisma.user.create({ 
-        data: {
-            name, 
-            email,
-            passwordHash,
-        }
-    })
-    const newUser = ({
-        id: createdUser.id,
-        name : createdUser.name,
-        email : createdUser.email,
-        role: createdUser.role,
-    })
-    res.json(newUser)
-})
+router.use('/auth/', userRouter)
 
 export default router
