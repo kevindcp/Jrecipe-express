c0b8ef57ea12e7cf2ed97fd43f3f8fa19de52783
kevindcp <13-10268@usb.ve>
2021-11-08 01:39:30 -0400

    added user routes

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,9 +1,106 @@
 import { Response, Request } from "express"
 import { PrismaClient, Prisma } from '@prisma/client'
+import { threadId } from "worker_threads"
 
 const prisma = new PrismaClient()
 
 export const getAll = async (req : Request, res : Response) => {
-    const users = await prisma.user.findMany()
-    res.json(users)
+    const users = await prisma.user.findMany({
+        select : {
+            email : true,
+            name: true, 
+            role: true,
+            recipes: true
+        }
+    })
+    res.status(200).json(users)
+}
+
+export const getUser = async(req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            },
+            select : {
+                email : true,
+                name: true, 
+                role: true,
+                recipes: true
+            }
+        })
+        res.status(200).json(user)
+    } catch (err) { 
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const updateUser = async(req: Request, res: Response) => {
+    try { 
+        const id = parseInt(req.params.id)
+        const {email, name, role} = req.body
+        const original = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+        if (!original) throw new Error('User id not valid')
+        const updated = await prisma.user.update({
+            where : {
+                id
+            },
+            data: {
+                email,
+                name,
+                role,
+            }
+        })
+        res.status(200).json("Update sucessful")
+    } catch(err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const deleteUser = async(req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        if (id == 1) throw new Error('Cant delete admin')
+        await prisma.user.delete({
+            where: {
+                id
+            }
+        })
+        res.status(200).json("Deletion successful")
+    } catch(err) { 
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const getSelf = async(req: Request, res: Response) => {
+    try {
+        const { decodedToken } = req.body
+        const user = await prisma.user.findUnique({
+            where : {
+                id: decodedToken.id
+            }, 
+            select : {
+                email : true,
+                name: true, 
+                role: true,
+                recipes: true
+            }
+        })
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
 }
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,10 +1,18 @@
 import express from "express"
-import { getAll } from "../controllers/users"
+import { deleteUser, getAll, getSelf, getUser, updateUser } from "../controllers/users"
 import { decodeToken } from "../middleware/decodeToken"
 import { isAdmin } from "../middleware/isAdmin"
 
 const userRouter = express.Router()
 
 userRouter.get('/', decodeToken, isAdmin, getAll)
+
+userRouter.get('/me', decodeToken, getSelf)
+
+userRouter.get('/:id', decodeToken, isAdmin, getUser)
+
+userRouter.patch('/:id', decodeToken, isAdmin, updateUser)
+
+userRouter.delete('/:id', decodeToken, isAdmin, deleteUser)
 
 export default userRouter
