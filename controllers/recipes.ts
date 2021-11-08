5d42d79217b5ad0f8ce80ec5bcc19e58c47d8597
kevindcp <13-10268@usb.ve>
2021-11-08 16:40:26 -0400

    recipe routes done

--- controllers/recipes.ts
+++ controllers/recipes.ts
@@ -13,3 +13,99 @@ export const getAll = async(req: Request, res: Response) => {
         })
     }
 }
+
+
+export const getRecipe = async(req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const recipe = await prisma.recipe.findUnique({
+            where: { 
+                id
+            }
+        })
+        res.status(200).json(recipe)
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const postRecipe = async(req: Request, res: Response) => {
+    try {
+        const { title, ingredients, steps, decodedToken } = req.body
+        const authorId = decodedToken.id
+        await prisma.recipe.create({
+            data: {
+                title,
+                ingredients,
+                steps,
+                authorId
+            }
+        })
+        res.status(200).json("Success")
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const updateRecipe = async(req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const { title, ingredients, steps, decodedToken } = req.body
+        const authorId = decodedToken.id 
+        const original = await prisma.recipe.findUnique({ 
+            where: { 
+                id
+            }
+        })
+        // Recipe does not exist
+        if (!original) return res.status(400).json({ error: 'No such recipe' })
+        // Only admin and creator of a recipe can update it
+        if (original.authorId != authorId && decodedToken.role != 'ADMIN') return res.status(401).json({ error: 'Not authorized' })
+        await prisma.recipe.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                ingredients,
+                steps
+            }
+        })
+        res.status(200).json("Sucess")
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+}
+
+export const deleteRecipe = async(req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const { decodedToken } = req.body
+        const authorId = decodedToken.id 
+        const original = await prisma.recipe.findUnique({ 
+            where: { 
+                id
+            }
+        })
+        // Recipe does not exist
+        if (!original) return res.status(400).json({ error: 'No such recipe' })
+        // Only admin and creator of a recipe can delete it
+        if (original.authorId != authorId && decodedToken.role != 'ADMIN') return res.status(401).json({ error: 'Not authorized' })
+        await prisma.recipe.delete({
+            where: {
+                id
+            }
+        })
+        res.status(200).json("Success")
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+}
--- routes/recipeRoutes.ts
+++ routes/recipeRoutes.ts
@@ -1,9 +1,18 @@
 import express from "express"
-import { getAll } from "../controllers/recipes"
+import { deleteRecipe, getAll, getRecipe, postRecipe, updateRecipe } from "../controllers/recipes"
+import { decodeToken } from "../middleware/decodeToken"
 
 
 const recipeRouter = express.Router()
 
 recipeRouter.get('/', getAll)
+
+recipeRouter.get('/:id', getRecipe)
+
+recipeRouter.post('/', decodeToken, postRecipe)
+
+recipeRouter.patch('/:id', decodeToken, updateRecipe)
+
+recipeRouter.delete('/:id', decodeToken, deleteRecipe)
 
 export default recipeRouter
