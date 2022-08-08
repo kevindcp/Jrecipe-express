99f3f71786ff7de66a4d5b83fcc90b2f46b75294
kevindcp <13-10268@usb.ve>
2022-08-08 13:34:17 -0400

    Changed  schema

---
+++ prisma/migrations/20220808155351_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "image" TEXT NOT NULL DEFAULT E'https://patch.com/img/cdn20/users/22915293/20200125/054306/styles/patch_image/public/missing___25173818654.jpg';
